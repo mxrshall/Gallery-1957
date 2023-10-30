83b54de62ef6945ba19c4a6856c00ca2e0bba234
mxrshall <martin22.krajcovic@gmail.com>
2023-10-30 19:27:37 +0100

    Vyuzitie useRef funkcie na scrollovanie v dive

--- src/Galeria.jsx
+++ src/Galeria.jsx
@@ -1,14 +1,19 @@
 import { GaleriaText } from "./GaleriaText";
 import Nadpis from "./Nadpis";
+import { useRef } from "react";
 import { motion, useScroll } from "framer-motion";
 
 function Galeria() {
-  const { scrollYProgress } = useScroll();
+  const ref = useRef(null);
+  const { scrollYProgress } = useScroll({ container: ref });
 
   return (
     <div className="bg-black flex justify-center items-center flex-col">
       <Nadpis nadpis="Galéria 1957" color="white" />
-      <div className="w-3/4 h-64 text-xs font-julius text-justify text-white overflow-auto">
+      <div
+        ref={ref}
+        className="w-3/4 h-64 text-xs font-julius text-justify text-white overflow-auto"
+      >
         <GaleriaText />
       </div>
       <motion.div
