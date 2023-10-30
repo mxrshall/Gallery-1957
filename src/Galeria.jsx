8656673221b6a78851ab82b67da5044ab3126ef3
mxrshall <martin22.krajcovic@gmail.com>
2023-10-30 16:30:20 +0100

    Tvorba scroll baru

--- src/Galeria.jsx
+++ src/Galeria.jsx
@@ -1,26 +1,20 @@
+import { GaleriaText } from "./GaleriaText";
 import Nadpis from "./Nadpis";
-import image1 from "./images/image1.png";
+import { motion, useScroll } from "framer-motion";
 
 function Galeria() {
+  const { scrollYProgress } = useScroll();
+
   return (
     <div className="bg-black flex justify-center items-center flex-col">
-      <Nadpis nadpis="Galéria" color="white" />
+      <Nadpis nadpis="Galéria 1957" color="white" />
       <div className="w-3/4 h-64 text-xs font-julius text-justify text-white overflow-auto">
-        Lorem Ipsum is simply dummy text of the printing and typesetting
-        industry. Lorem Ipsum has been the industry's standard dummy text ever
-        since the 1500s, when an unknown printer took a galley of type and
-        scrambled it to make a type specimen book. It has survived not only five
-        centuries, but also the leap into electronic typesetting, remaining
-        essentially unchanged. It was popularised in
-        <img className="w-full h-42 py-5" src={image1}></img>
-        Lorem Ipsum is simply dummy text of the printing and typesetting
-        industry. Lorem Ipsum has been the industry's standard dummy text ever
-        since the 1500s, when an unknown printer took a galley of type and
-        scrambled it to make a type specimen book. It has survived not only five
-        centuries, but also the leap into electronic typesetting, remaining
-        essentially unchanged. It was popularised in
-        <img className="w-full h-42 py-5" src={image1}></img>
+        <GaleriaText />
       </div>
+      <motion.div
+        className="w-3/4 h-10 bg-red-500 origin-[0%]"
+        style={{ scaleX: scrollYProgress }}
+      ></motion.div>
     </div>
   );
 }
---
+++ src/GaleriaText.jsx
@@ -0,0 +1,22 @@
+import image1 from "./images/image1.png";
+
+export function GaleriaText() {
+  return (
+    <article>
+      Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+      Lorem Ipsum has been the industry's standard dummy text ever since the
+      1500s, when an unknown printer took a galley of type and scrambled it to
+      make a type specimen book. It has survived not only five centuries, but
+      also the leap into electronic typesetting, remaining essentially
+      unchanged. It was popularised in
+      <img className="w-full h-42 py-5" src={image1}></img>
+      Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+      Lorem Ipsum has been the industry's standard dummy text ever since the
+      1500s, when an unknown printer took a galley of type and scrambled it to
+      make a type specimen book. It has survived not only five centuries, but
+      also the leap into electronic typesetting, remaining essentially
+      unchanged. It was popularised in
+      <img className="w-full h-42 py-5" src={image1}></img>
+    </article>
+  );
+}
