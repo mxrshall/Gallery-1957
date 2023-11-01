0863bba575b6a75f288b27e9ee078c66fdb3d78f
mxrshall <martin22.krajcovic@gmail.com>
2023-11-01 02:12:51 +0100

    Doplnenie textu do footeru

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -1,7 +1,24 @@
 function Footer() {
   return (
     <footer className="w-full h-64 bg-black">
-      <div></div>
+      <div className="w-4/5 h-32 pl-3 pt-3">
+        <h1 className="text-white text-lg font-jost">Galéria 1957</h1>
+        <p className="text-white text-xs font-julius">
+          Galéria súčasného umenia s priestormi v Accre, Ghane a Londýne,
+          Spojené kráľovstvo.
+        </p>
+      </div>
+      <div className="w-full h-32 pl-3 pt-3">
+        <span className="text-white text-xs font-julius">
+          Gallery 1957, London
+          <br />
+          1 Hyde Park Gate
+          <br />
+          London
+          <br />
+          london@gallery1957.com
+        </span>
+      </div>
     </footer>
   );
 }
