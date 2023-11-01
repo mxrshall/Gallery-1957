e4aa688c32f50b82ae6131fd276e5e1e0ba2b6fe
mxrshall <martin22.krajcovic@gmail.com>
2023-11-01 15:07:25 +0100

    Dynamicke pridavanie obrazkov

--- src/Artists.jsx
+++ src/Artists.jsx
@@ -3,12 +3,18 @@ import Footer from "./Footer";
 import Umelec from "./Umelec";
 import Header from "./Header";
 
+import annanaffotey from "./images/annanaffotey.jpeg";
+import juwonaderemi from "./images/juwonaderemi.jpg";
+import corneliusannor from "./images/corneliusannor.jpg";
+
 function Artists() {
   return (
     <>
       <Navigacia />
       <Header />
-      <Umelec />
+      <Umelec image={annanaffotey} />
+      <Umelec image={juwonaderemi} />
+      <Umelec image={corneliusannor} />
       <Footer />
     </>
   );
--- src/Umelec.jsx
+++ src/Umelec.jsx
@@ -1,9 +1,14 @@
-function Umelec() {
+function Umelec(props) {
   return (
     <div className="flex justify-center items-center flex-col">
-      <div className="w-full h-64 bg-[url('./images/annanaffotey.jpeg')] bg-cover grayscale"></div>
-      <div className="w-full h-64 bg-[url('./images/juwonaderemi.jpg')] bg-cover grayscale"></div>
-      <div className="w-full h-64 bg-[url('./images/corneliusannor.jpg')] bg-cover grayscale"></div>
+      <div
+        className="w-full h-64 bg-cover grayscale"
+        style={{
+          backgroundImage: `url(${props.image})`,
+        }}
+      >
+        <div className="w-full h-full bg-black opacity-60"></div>
+      </div>
     </div>
   );
 }
