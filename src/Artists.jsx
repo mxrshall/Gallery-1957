d94e5f29e24f34a203c2e8e2859ec5a8654ce7c2
mxrshall <martin22.krajcovic@gmail.com>
2023-11-02 01:19:58 +0100

    Doplnenie realneho textu

--- src/Artists.jsx
+++ src/Artists.jsx
@@ -14,17 +14,19 @@ function Artists() {
       <Header />
       <Umelec
         meno="ANNAN AFFOTEY"
-        popis="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has"
+        popis="Umelec z Oxfordu Annan Affotey naviguje černošskú identitu prostredníctvom portrétovania. Jeho obrazy sa zameriavajú na farebné ženy a mužov so žiarivou tmavou pokožkou a dušou naplnenými červenými očami. Svoje postavy obklopuje negatívnym priestorom a umožňuje každému subjektu rozprávať svoj vlastný príbeh."
         image={annanaffotey}
       />
       <Umelec
         meno="JUWON ADEREMI"
-        popis="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has"
+        popis="Juwon Aderemi je umelec narodený v Nigérii. Jeho prax sa primárne sústreďuje okolo surrealistických postáv a abstraktného realizmu na zobrazenie rozhovorov, ktoré sa zaoberajú kultúrou a presvedčením, na ktoré je prevažne zvyknutý. Pracuje v intuitívnom postupe, ktorý je primárne založený na portrétovaní, pričom uprednostňuje svoju nigérijskú kultúru."
         image={juwonaderemi}
       />
       <Umelec
         meno="CORNELIUS ANNOR"
-        popis="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has"
+        popis="Cornelius Annor je ghanský umelec, ktorý sa narodil v roku 1990.
+        Jeho práca sa v súčasnosti predvádzajú na viacerých miestach, ako je Maruani Mercier v Bruseli. V mnohých kľúčových galériách a múzeách, ako je Opera Gallery v Dubaji, sa v minulosti vystavovali diela Cornelia Annora.
+        Dielo Cornelia Annora bolo viackrát ponúkané na aukcii, pričom realizované ceny sa pohybujú od 20 696 USD do 94 500 USD."
         image={corneliusannor}
       />
       <Footer />
