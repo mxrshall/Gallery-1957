88b6227cb1aec5cd5d4b495b68eae6cb6b12e476
mxrshall <martin22.krajcovic@gmail.com>
2023-11-06 13:41:26 +0100

    Odstranenie scroll baru

--- src/Galeria.jsx
+++ src/Galeria.jsx
@@ -14,7 +14,7 @@ function Galeria() {
         <div className="w-1/12 h-64 lg:h-96"></div>
         <div
           ref={ref}
-          className="w-3/4 h-64 text-xs font-julius text-justify text-white overflow-auto lg:w-1/2 lg:h-96 lg:text-base"
+          className="w-3/4 h-64 text-xs font-julius text-justify text-white overflow-auto no-scrollbar lg:w-1/2 lg:h-96 lg:text-base"
         >
           <GaleriaText />
         </div>
