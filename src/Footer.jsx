7f60fa3f26b5d6e684ffb56ba264c231c62049ed
mxrshall <martin22.krajcovic@gmail.com>
2023-11-07 13:11:45 +0100

    Footer pre desktop

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -5,16 +5,18 @@ import social3 from "./images/social3.png";
 function Footer() {
   return (
     <footer className="w-full h-64 bg-black">
-      <div className="w-4/5 h-32 pl-3 pt-3">
-        <h1 className="text-white text-xl font-jost">Galéria 1957</h1>
-        <p className="text-white text-xs font-julius">
+      <div className="w-4/5 h-32 pl-3 pt-3 lg:pl-5 lg:pt-5">
+        <h1 className="text-white text-xl font-jost lg:text-3xl">
+          Galéria 1957
+        </h1>
+        <p className="text-white text-xs font-julius lg:text-base">
           Galéria súčasného umenia s priestormi v Accre, Ghane a Londýne,
           Spojené kráľovstvo.
         </p>
       </div>
       <div className="w-full h-32 flex flex-row">
-        <div className="w-1/2 h-32 pl-3 pt-3">
-          <span className="text-white text-xs font-julius">
+        <div className="w-1/2 h-32 pl-3 pt-3 lg:pl-5 ">
+          <span className="text-white text-xs font-julius lg:text-base">
             Gallery 1957, London
             <br />
             1 Hyde Park Gate
@@ -25,9 +27,9 @@ function Footer() {
           </span>
         </div>
         <div className="w-1/2 h-32 flex justify-end items-end pr-3 pb-3">
-          <img className="w-1/9 h-4 ml-1" src={social1}></img>
-          <img className="w-1/9 h-4 ml-1" src={social2}></img>
-          <img className="w-1/9 h-4 ml-1" src={social3}></img>
+          <img className="w-1/9 h-4 ml-1 lg:h-6 lg:ml-2" src={social1}></img>
+          <img className="w-1/9 h-4 ml-1 lg:h-6 lg:ml-2" src={social2}></img>
+          <img className="w-1/9 h-4 ml-1 lg:h-6 lg:ml-2" src={social3}></img>
         </div>
       </div>
     </footer>
