fa72dd7380e6d635020c5e7e1e97a64f91ea71cc
mxrshall <martin22.krajcovic@gmail.com>
2023-11-09 14:22:47 +0100

    News responzivne pre desktop

--- src/News.jsx
+++ src/News.jsx
@@ -14,20 +14,20 @@ function News() {
       <Header />
       <div className="w-full bg-white">
         <div
-          className="w-full h-48 bg-cover"
+          className="w-full h-48 bg-cover lg:h-[80vh]"
           style={{
             backgroundImage: `url(${data.image})`,
           }}
         ></div>
-        <div className="text-black text-base font-bold font-julius pt-3 pl-2">
+        <div className="text-black text-base font-bold font-julius pt-3 pl-2 lg:text-5xl lg:font-normal lg:pt-10 lg:pl-10">
           <h1>{data.name}</h1>
         </div>
-        <div className="text-black text-sm font-julius pt-3 pl-2">
+        <div className="text-black text-sm font-julius pt-3 pl-2 lg:pt-10 lg:pl-10 lg:text-lg">
           <h1>{data.description}</h1>
         </div>
         <div
           className="text-black text-xs font-julius pt-3
-        pb-3 pl-2"
+        pb-3 pl-2 lg:pt-5 lg:pl-10 lg:pb-10 lg:text-sm"
         >
           <p>{data.date}</p>
         </div>
