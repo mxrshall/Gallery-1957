c7bf671220ac4592c3b1c3b6fa8e0a80972b00dd
mxrshall <martin22.krajcovic@gmail.com>
2023-11-12 01:09:54 +0100

    Pridanie animacie do headeru

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,3 +1,5 @@
+import { motion } from "framer-motion";
+
 function Header() {
   return (
     <div>
@@ -5,15 +7,25 @@ function Header() {
         <div className="w-1/3 h-96 lg:h-[100vh]"></div>
         <div className="w-2/3 h-96 lg:h-[100vh] lg:pl-[20rem]">
           <div className="bg-black w-full h-96 flex justify-center items-center pb-15 lg:h-[100vh]">
-            <span className="bg-clip-text text-transparent text-[12rem] leading-[9rem] bg-[url('./images/background.png')] font-jost bg-fixed lg:text-[30rem] lg:leading-[50vh]">
+            <motion.span
+              className="bg-clip-text text-transparent text-[12rem] leading-[9rem] bg-[url('./images/background.png')] font-jost bg-fixed lg:text-[30rem] lg:leading-[50vh]"
+              animate={{ opacity: 1 }}
+              initial={{ opacity: 0 }}
+              transition={{ delay: 0.25, duration: 0.8 }}
+            >
               19
               <br />
               57
-            </span>
+            </motion.span>
           </div>
         </div>
         <div className="w-full h-96 absolute flex items-end z-10 pl-8 pb-12 lg:h-[100vh]">
-          <span className="text-white text-xl font-julius lg:text-[5rem] lg:leading-[10vh]">
+          <motion.span
+            className="text-white text-xl font-julius lg:text-[5rem] lg:leading-[10vh]"
+            animate={{ x: 0, opacity: 1 }}
+            initial={{ x: -25, opacity: 0 }}
+            transition={{ delay: 0.8, duration: 0.8 }}
+          >
             Galéria predstavuje
             <br />
             predných umelcov
@@ -21,7 +33,7 @@ function Header() {
             pracujúcich v západanej
             <br />
             afrike
-          </span>
+          </motion.span>
         </div>
       </div>
     </div>
