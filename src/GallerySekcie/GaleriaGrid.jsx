8afcc8087dd6d5bdf9d932bf47725196bdafaac3
mxrshall <martin22.krajcovic@gmail.com>
2023-11-17 01:40:26 +0100

    Vytvorenie animacie na podstranke Galeria

--- src/GallerySekcie/GaleriaGrid.jsx
+++ src/GallerySekcie/GaleriaGrid.jsx
@@ -1,7 +1,35 @@
+import { useRef, useEffect } from "react";
+import { motion, useAnimation, useInView } from "framer-motion";
+
 function GaleriaGrid(props) {
   const submit = (data) => {
     props.submitData(data);
   };
+
+  const ref = useRef(null);
+  const isInView = useInView(ref, { once: true });
+  const control = useAnimation();
+
+  const left = {
+    hidden: { opacity: 0, x: 100 },
+    visible: { opacity: 1, x: 0 },
+  };
+
+  const right = {
+    hidden: { opacity: 0, x: -100 },
+    visible: { opacity: 1, x: 0 },
+  };
+
+  const mid = {
+    hidden: { opacity: 0, scale: 0.8 },
+    visible: { opacity: 1, scale: 1 },
+  };
+
+  useEffect(() => {
+    if (isInView) {
+      control.start("visible");
+    }
+  }, [isInView]);
 
   return (
     <>
@@ -25,30 +53,51 @@ function GaleriaGrid(props) {
           }}
         ></div>
       </div>
-      <div className="hidden w-full h-full lg:grid grid-cols-3 grid-rows-2 gap-5">
-        <div
+      <div
+        ref={ref}
+        className="hidden w-full h-full lg:grid grid-cols-3 grid-rows-2 gap-5"
+      >
+        <motion.div
+          variants={left}
+          initial="hidden"
+          animate={control}
+          transition={{ duration: 0.8, delay: 0.5 }}
           className="row-span-2 bg-cover bg-no-repeat"
           style={{
             backgroundImage: `url(${props.foto1})`,
           }}
-        ></div>
-        <div className="row-span-2 flex items-center justify-center flex-col font-julius text-3xl cursor-default">
+        ></motion.div>
+        <motion.div
+          variants={mid}
+          initial="hidden"
+          animate={control}
+          transition={{ duration: 0.6, delay: 1 }}
+          className="row-span-2 flex items-center justify-center flex-col font-julius text-3xl cursor-default"
+        >
           <p onClick={() => submit(props.foto1)}>{props.nazov1}</p>
           <p onClick={() => submit(props.foto2)}>{props.nazov2}</p>
           <p onClick={() => submit(props.foto3)}>{props.nazov3}</p>
-        </div>
-        <div
+        </motion.div>
+        <motion.div
+          variants={right}
+          initial="hidden"
+          animate={control}
+          transition={{ duration: 0.8, delay: 0.5 }}
           className="bg-cover bg-no-repeat"
           style={{
             backgroundImage: `url(${props.foto2})`,
           }}
-        ></div>
-        <div
+        ></motion.div>
+        <motion.div
+          variants={right}
+          initial="hidden"
+          animate={control}
+          transition={{ duration: 0.8, delay: 0.5 }}
           className="col-start-3 row-start-2 bg-cover bg-no-repeat"
           style={{
             backgroundImage: `url(${props.foto3})`,
           }}
-        ></div>
+        ></motion.div>
       </div>
     </>
   );
